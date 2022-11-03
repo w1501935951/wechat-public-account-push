8bd759b7ca2e48883ed3d44819ac5252dfa097a5
w1501935951 <38433828+w1501935951@users.noreply.github.com>
2022-11-03 22:32:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -5,62 +5,7 @@
  * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
  * 自行添加属性，以支持更多个性化功能
  */
-const USER_CONFIG = {
-
-  // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
-
-  // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
-
-  PROVINCE: '广东',
-  CITY: '惠州',
-
-  USERS: [
-    {
-      // 想要发送的人的名字
-      name: '宝贝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-  ],
-
-
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
-
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-    }
-  ],
-
+const USER_CONFIG = {{"APP_ID":"wx3dfada4aa29f3fbf","APP_SECRET":"cb729fc650d83ffc0f85cb19f65c1189","IS_SHOW_COLOR":true,"CALLBACK_TEMPLATE_ID":"","CALLBACK_USERS":[{"name":"自己","id":""}],"USERS":[{"name":"坤坤","id":"ohMXa52KPtVeFMWnDsHLitDVB1H4","useTemplateId":"u9sadJIddF8EEcIpGDcJrArvRw3LApUqwMt9Xu3Us3Y","province":"陕西","city":"西安市","horoscopeDate":"01-28","horoscopeDateType":"今日","openUrl":"https://m.niucodata.com/cat/cat.php","festivals":[{"type":"*生日","name":"懒大王","date":"01-28","year":"2002"}],"customizedDateList":[]}],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":true,"oneTalk":false,"earthyLoveWords":false,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":false,"birthdayMessage":true}}
 }
 
 module.exports = USER_CONFIG
