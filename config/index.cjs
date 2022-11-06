1ef75f02e6ce5130a1bc25d473ca10226e42ef60
w1501935951 <38433828+w1501935951@users.noreply.github.com>
2022-11-07 00:24:48 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -5,7 +5,7 @@
  * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
  * 自行添加属性，以支持更多个性化功能
  */
-const USER_CONFIG = {"APP_ID":"wx3dfada4aa29f3fbf","APP_SECRET":"cb729fc650d83ffc0f85cb19f65c1189","IS_SHOW_COLOR":true,"CALLBACK_TEMPLATE_ID":"","CALLBACK_USERS":[{"name":"自己","id":""}],"USERS":[{"name":"坤坤","id":"ohMXa52KPtVeFMWnDsHLitDVB1H4","useTemplateId":"u9sadJIddF8EEcIpGDcJrArvRw3LApUqwMt9Xu3Us3Y","province":"陕西","city":"西安","horoscopeDate":"01-28","horoscopeDateType":"今日","openUrl":"https://m.niucodata.com/cat/cat.php","festivals":[{"type":"*生日","name":"懒大王","date":"01-28","year":"2002"}],"customizedDateList":[]}],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":true,"oneTalk":false,"earthyLoveWords":false,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":false,"birthdayMessage":true}}
+const USER_CONFIG = {"APP_ID":"wx3dfada4aa29f3fbf","APP_SECRET":"cb729fc650d83ffc0f85cb19f65c1189","IS_SHOW_COLOR":true,"CALLBACK_TEMPLATE_ID":"","CALLBACK_USERS":[{"name":"自己","id":""}],"USERS":[{"name":"坤坤","id":"ohMXa52KPtVeFMWnDsHLitDVB1H4","useTemplateId":"u9sadJIddF8EEcIpGDcJrArvRw3LApUqwMt9Xu3Us3Y","province":"陕西","city":"西安","horoscopeDate":"01-28","horoscopeDateType":"今日","openUrl":"https://m.niucodata.com/cat/cat.php","festivals":[{"type":"*生日","name":"懒大王","date":"01-28","year":"2002"}],"customizedDateList":[]}],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":true,"oneTalk":true,"earthyLoveWords":false,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":false,"birthdayMessage":true}}
 
 
 module.exports = USER_CONFIG
